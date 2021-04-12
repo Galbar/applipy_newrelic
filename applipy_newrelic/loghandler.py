68627b731f9c084d66512949258f564fbe642280
Alessio Linares <gitlab@alessio.cc>
2021-04-12 10:02:07 +0200

    add app_name to logs

--- applipy_newrelic/loghandler.py
+++ applipy_newrelic/loghandler.py
@@ -1,5 +1,7 @@
 from logging import Handler
 
+from applipy import Config
+
 from newrelic_telemetry_sdk import (
     Log,
     LogClient,
@@ -10,9 +12,12 @@ class NewRelicLogHandler(Handler):
 
     _client: LogClient
 
-    def __init__(self, client: LogClient) -> None:
+    def __init__(self, client: LogClient, config: Config) -> None:
         super().__init__()
         self._client = client
+        self._app_name = config.get('app.name')
 
     def emit(self, record):
-        self._client.send(Log.from_record(record)).raise_for_status()
+        log = Log.from_record(record)
+        log['app_name'] = self._app_name
+        self._client.send(log).raise_for_status()
