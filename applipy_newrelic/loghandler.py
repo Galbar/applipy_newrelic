59dc1f1675dc1e7385db30743a73b284ad7b1fd5
Alessio Linares <gitlab@alessio.cc>
2021-04-17 19:13:48 +0200

    add hostname to logs

--- applipy_newrelic/loghandler.py
+++ applipy_newrelic/loghandler.py
@@ -1,3 +1,4 @@
+import platform
 from logging import Handler
 
 from applipy import Config
@@ -16,8 +17,10 @@ class NewRelicLogHandler(Handler):
         super().__init__()
         self._client = client
         self._app_name = config.get('app.name')
+        self._hostname = platform.node()
 
     def emit(self, record):
         log = Log.from_record(record)
         log['app_name'] = self._app_name
+        log['hostname'] = self._hostname
         self._client.send(log).raise_for_status()
